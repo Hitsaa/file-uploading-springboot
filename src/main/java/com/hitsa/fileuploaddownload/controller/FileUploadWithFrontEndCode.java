208cd97369575d31b6b102b814fda17de85cdbd8
hitsaa <sshitendra010@gmail.com>
2021-05-14 18:57:13 +0530

    single file upload and download

---
+++ src/main/java/com/hitsa/fileuploaddownload/controller/FileUploadWithFrontEndCode.java
@@ -0,0 +1,21 @@
+package com.hitsa.fileuploaddownload.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author - GreenLearner(https://www.youtube.com/c/greenlearner)
+ */
+
+@Controller
+public class FileUploadWithFrontEndCode {
+
+    @GetMapping("/files")
+    ModelAndView fileUpload(){
+        Model m = new ExtendedModelMap();
+        return new ModelAndView("index.html");
+    }
+}
