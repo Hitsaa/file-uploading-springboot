f0cc08430fd5a40c83531818110f0c6b685d8a3f
hitsaa <sshitendra010@gmail.com>
2021-05-14 18:48:57 +0530

    resolving connection issue

--- src/test/java/com/hitsa/fileuploaddownload/FileuploaddownloadApplicationTests.java
+++ src/test/java/com/hitsa/fileuploaddownload/FileuploaddownloadApplicationTests.java
@@ -1,10 +1,10 @@
-package com.greenlearner.fileUploadDownload;
+package com.hitsa.fileuploaddownload;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FileUploadDownloadApplicationTests {
+class FileuploaddownloadApplicationTests {
 
 	@Test
 	void contextLoads() {
